ac2c118aeedb12ee98081d23e3db0567a02a3470
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-09-23 16:01:15 -0400

    feat(write): create a function in helper file to write the note on db

--- helpers/fsUtil.js
+++ helpers/fsUtil.js
@@ -4,3 +4,8 @@ const util = require('util');
 // promisify the fs module
 // Used(https://nodejs.org/api/util.html#utilpromisifyoriginal) to learn about promisify.
 const readFileAsync = util.promisify(fs.readFile);
+
+const writeToFile = (dest, data) => 
+  fs.writeFile(dest, JSON.stringify(data, null, 2), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${dest}`)
+  );
