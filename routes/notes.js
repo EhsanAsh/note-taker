b0c634249ad624ad0fde28b940aa90f0254f6a19
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-09-23 18:47:44 -0400

    feat(noteGet): create a route to send the db content as the response to get request

--- routes/notes.js
+++ routes/notes.js
@@ -7,3 +7,13 @@ const { readFromFile, readAndAppend } = require('../helpers/fsUtil');
 const { v4: uuidv4 } = require('uuid');
 
 // GET Route for retrieving all the notes
+notes.get('/', (req, res) => { 
+  readFromFile('/db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+notes.post('/', (req, res) => { 
+  console.log(req.body);
+});
+
+module.exports = notes;
