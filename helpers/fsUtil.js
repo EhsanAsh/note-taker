e8331523528c4bbddddb86dd06658325a40fa1be
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-09-23 16:36:43 -0400

    feat(read): create fs readFile to read the data and write it inside the db

--- helpers/fsUtil.js
+++ helpers/fsUtil.js
@@ -3,9 +3,23 @@ const util = require('util');
 
 // promisify the fs module
 // Used(https://nodejs.org/api/util.html#utilpromisifyoriginal) to learn about promisify.
-const readFileAsync = util.promisify(fs.readFile);
+const readFromFile = util.promisify(fs.readFile);
 
-const writeToFile = (dest, data) => 
-  fs.writeFile(dest, JSON.stringify(data, null, 2), (err) =>
+const writeToFile = (dest, content) => 
+  fs.writeFile(dest, JSON.stringify(content, null, 2), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${dest}`)
   );
+
+const readAndAppend = (content, src) => { 
+  fs.readFile(src, 'utf8', (err, data) => { 
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(src, parsedData);
+    }
+  });
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend };
